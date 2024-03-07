457c91b5563402559ae4a5113eec3bfc665b7a99
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-07 10:01:55 +0100

    Add header component, change page background to black

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import Header from "./Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+);
