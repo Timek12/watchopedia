2485c3ac41b1861199e017675136c96b82021af9
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-07 10:29:24 +0100

    Add counter state and functions for decrementing and incrementing current state

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 const Counter = () => {
+  const [counter, setCounter] = useState(() => {
+    return 10;
+  });
+
+  function incrementCounter() {
+    setCounter((previousState) => {
+      return previousState + 1;
+    });
+  }
+
+  function decrementCounter() {
+    setCounter((previousState) => {
+      return previousState - 1;
+    });
+  }
+
   return (
     <div className="col-12 col-md-4 offset-md-4 border text-white">
       <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span>
-      <button className="btn btn-success m-1">+1</button>
-      <button className="btn btn-danger m-1">-1</button>
-    <br/>
-    <span className="h4">
+      <button className="btn btn-success m-1" onClick={incrementCounter}>
+        +1
+      </button>
+      <button className="btn btn-danger m-1" onClick={decrementCounter}>
+        -1
+      </button>
+      <br />
+      <span className="h4">
         Counter: &nbsp;
-        <span className="text-success">0</span>
-    </span>
+        <span className="text-success">{counter}</span>
+      </span>
     </div>
   );
 };
