f25d4d0a594d1f9553799f16c2fb03e9da375400
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-07 16:37:58 +0100

    Fix naming of movieList

--- src/MovieComponents/MovieList.jsx
+++ src/MovieComponents/MovieList.jsx
@@ -1,7 +1,7 @@
 const MovieList = (props) => {
   return (
     <div className="text-white">
-      {props.moveList.map((movie, index) => {
+      {props.movieList.map((movie, index) => {
         return <div key={index}>{movie}</div>;
       })}
     </div>
