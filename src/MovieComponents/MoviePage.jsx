0f4553030204ea460ac32bd46b2c9e4260e2c14e
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-07 16:39:32 +0100

    Add fucntionality to handle add movie, pass method that changes the state by adding new movie name as a parameter to AddMovie component, and then when form is submitted call this method

--- src/MovieComponents/MoviePage.jsx
+++ src/MovieComponents/MoviePage.jsx
@@ -6,10 +6,20 @@ const MoviePage = () => {
   const [movieState, setMovieState] = useState(() => {
     return { movieList: ["Die Hard", "Harry Potter"] };
   });
+
+  const handleAddMovie = (movieName) => {
+    setMovieState((previousState) => {
+      return {
+        ...previousState,
+        movieList: previousState.movieList.concat([movieName]),
+      };
+    });
+  };
+
   return (
     <div className="container col-12 col-md-6 my-3 border">
-      <AddMovie />
-      <MovieList moveList={movieState.movieList}/>
+      <AddMovie handleAddMovie={handleAddMovie} />
+      <MovieList movieList={movieState.movieList} />
     </div>
   );
 };
